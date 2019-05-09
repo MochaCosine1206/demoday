c8ee043e1a661178bd923006e87e02074bd25385
StevenXPS13 <steven.parrott@gmail.com>
2019-05-09 10:10:08 -0700

    Updates

---
+++ precache-manifest.ca9896ef684eaeb4017d220feb4845e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62fe00e2c869e1a4242a5715fa2fc5f1",
+    "url": "/demoday/index.html"
+  },
+  {
+    "revision": "4b3148094c7f8a57e6c9",
+    "url": "/demoday/static/css/main.de4faedd.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/demoday/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4b3148094c7f8a57e6c9",
+    "url": "/demoday/static/js/main.2dc013d9.chunk.js"
+  },
+  {
+    "revision": "ce820537fec6ff8e72fa",
+    "url": "/demoday/static/js/runtime~main.681ce6b2.js"
+  },
+  {
+    "revision": "102a81008bc0b461878f14648736325b",
+    "url": "/demoday/static/media/stevePortraitB.102a8100.svg"
+  }
+]);
